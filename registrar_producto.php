2b749352ae8493fe4c60081a74bad9d9ea9e147b
empanadass <119280241+empanadass@users.noreply.github.com>
2023-10-17 04:55:16 -0400

    Update registrar_producto.php

--- registrar_producto.php
+++ registrar_producto.php
@@ -1,9 +1,9 @@
 <?php
 include 'conexion.php';
 $nombre=$_POST['nombre'];
-$categoria=$_POST['categoria'];
+
 $precio=$_POST['precio'];
-$mysql = "INSERT INTO producto (N_PRODUCTO, ID_CATPRODUCTO, PRECIO_UNI) VALUES ('$nombre', '$categoria', '$precio')";
+$mysql = "INSERT INTO producto (N_PRODUCTO, PRECIO_UNI) VALUES ('$nombre',  '$precio')";
 mysqli_query($conexion,$mysql) or die (mysqli_connect_error());
 mysqli_close($conexion);
 ?>
